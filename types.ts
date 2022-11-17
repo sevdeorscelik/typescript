7ddcaf582f3585b274259e9f66a4e5cff5843edd
Sevde <sevdeorscelik@gmail.com>
2022-11-17 22:32:35 +0100

    types

---
+++ types.ts
@@ -0,0 +1,13 @@
+/*
+let number = 5;
+
+number = "a";  
+//js'de tanimli bir variable'a baska bir deger ile tipini degistirebiliyoruz. ts buna izin vermiyor
+
+*/
+
+/*let a;*/ //tipi any olarak atanir ve istedigimiz tipi verebilirz
+a = 5;
+a = 'a';
+a = true;
+
